35084f6be11bf9a61283b031612fe36c9cd439c2
Steven Cabrera <stevencabrerasuarez135@gmail.com>
2025-06-29 20:55:33 -0600

    primer commit: Se creo el repositorio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+ArrayList lista = new();
+Console.Write("Ingrese un numero separados por espacio: ");
+lista.AddRange(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
+lista.Sort();
+Console.WriteLine($"Maximo: {((int[])lista.ToArray(typeof(int))).Max()}");
+Console.WriteLine($"Minimo: {((int[])lista.ToArray(typeof(int))).Min()}");
+
