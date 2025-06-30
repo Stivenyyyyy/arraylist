1c29177928882b99ce728e9cf6602ca859578551
Steven Cabrera <stevencabrerasuarez135@gmail.com>
2025-06-29 21:15:54 -0600

    cuarto commit: se agrego otro comentario

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections;
 //El siguiente programa lee cinco numeros y al final indica el mayor y el menor 
+//Y la suma de los elementos
 ArrayList lista = new();
 Console.Write("Ingrese un numero separados por espacio: ");
 lista.AddRange(Array.ConvertAll(Console.ReadLine().Split(), int.Parse));
 lista.Sort();
 Console.WriteLine($"Maximo: {((int[])lista.ToArray(typeof(int))).Max()}");
 Console.WriteLine($"Minimo: {((int[])lista.ToArray(typeof(int))).Min()}");
-
+Console.WriteLine($"Suma: {lista.OfType<int>().Sum()}");
